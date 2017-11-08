cc33c056a3211c3c1f7f0522e030aa19a6dbc416
Ahmed ElSayed <ahmels@microsoft.com>
2017-11-08 14:28:34 -0800

    Adding functions-runtime

---
+++ functions-runtime/init_container.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cat >/etc/motd <<EOL 
+  _____                               
+  /  _  \ __________ _________   ____  
+ /  /_\  \\___   /  |  \_  __ \_/ __ \ 
+/    |    \/    /|  |  /|  | \/\  ___/ 
+\____|__  /_____ \____/ |__|    \___  >
+        \/      \/                  \/ 
+          F U N C T I O N S
+
+EOL
+cat /etc/motd
+
+dotnet functions-runtime/Microsoft.Azure.WebJobs.Script.WebHost.dll
